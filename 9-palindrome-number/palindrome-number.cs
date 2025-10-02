49b0fe16ed2230215cea7574f54a0ee6cf51079b
Jakub Valek <kubo.valek@gmail.com>
2025-10-02 12:16:20 +0200

    Time: 15 ms (11.35%) | Memory: 39.4 MB (12.97%) - LeetSync

--- 9-palindrome-number/palindrome-number.cs
+++ 9-palindrome-number/palindrome-number.cs
@@ -1,13 +1,7 @@
 public class Solution {
     public bool IsPalindrome(int x) {
-        // if is negative it is not palindrome
-        if (x < 0) {
-            return false;
-        }
-
         String s = x.ToString();
         String rev = new String(s.Reverse().ToArray());
         return s == rev;
-
     }
 }
