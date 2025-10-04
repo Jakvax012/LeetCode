038aa482831823e42559e61eab6b06d785e65431
Jakub Valek <kubo.valek@gmail.com>
2025-10-04 21:28:06 +0200

    Time: 3 ms (76.53%) | Memory: 46.6 MB (63.92%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cs
+++ 12-integer-to-roman/integer-to-roman.cs
@@ -1,33 +1,33 @@
 public class Solution {
     public string IntToRoman(int num) {
-        var map = new (int value, string symbol)[]
-    {
-        (1000, "M"),
-        (900,  "CM"),
-        (500,  "D"),
-        (400,  "CD"),
-        (100,  "C"),
-        (90,   "XC"),
-        (50,   "L"),
-        (40,   "XL"),
-        (10,   "X"),
-        (9,    "IX"),
-        (5,    "V"),
-        (4,    "IV"),
-        (1,    "I")
-    };
-
-    var result = new System.Text.StringBuilder();
-
-    foreach (var (value, symbol) in map)
-    {
-        while (num >= value)
+        var map = new (int numberArb, string numberRim)[] 
         {
-            result.Append(symbol);
-            num -= value;
-        }
-    }
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I")
+        };
 
-    return result.ToString();
+        var result = new System.Text.StringBuilder();
+
+        foreach (var (number, symbol) in map) 
+        {
+            while (number <= num) 
+            {
+                result.Append(symbol);
+                num -= number;
+            }
+        }
+
+        return result.ToString();
     }
 }
